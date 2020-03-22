0208f9d5da04e8a9a36f51811e675d3f7672001c
EvyaB <evyatarbittonwork@gmail.com>
2020-03-22 11:08:26 +0200

    First commit of project

---
+++ ViewModel/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MySchoolYear.Model;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MySchoolYear.ViewModel
+{
+    public class UserViewModel
+    {
+        public List<User> myUsers
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            schoolEntities dbContext = new schoolEntities();
+            myUsers = dbContext.Users.ToList();
+        }
+    }
+}
