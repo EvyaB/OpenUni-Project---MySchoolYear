f8fd5551e9d317a46485b7096ab246621b584f35
EvyaB <evyatarbittonwork@gmail.com>
2020-03-22 20:37:50 +0200

    Setting an index in the Users Table, updating location of RelayCommand and Creating a viewmodel for the login window

---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MySchoolYear.Model;
+using MySchoolYear.Utilities;
+
+namespace MySchoolYear.ViewModel
+{
+    /// <summary>
+    /// ViewModel for the login process logic.
+    /// </summary>
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public RelayCommand LoginCommand 
+        { 
+            get;
+            set;
+        }
+
+        private bool AttemptLoginCommand()
+        {
+            schoolEntities mySchool = new schoolEntities();
+            var myAccount = mySchool.Users.SingleOrDefault(user => user.username == Username && user.password == Password);
+
+            if (myAccount != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
